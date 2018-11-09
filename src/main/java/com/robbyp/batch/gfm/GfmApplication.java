a4fe08b5754a55a9d992a054a2598d50cee79747
Rob Purcell <rob.purcell@gmail.com>
2018-11-09 17:42:00 +0000

    Initial commit

---
+++ src/main/java/com/robbyp/batch/gfm/GfmApplication.java
@@ -0,0 +1,12 @@
+package com.robbyp.batch.gfm;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GfmApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GfmApplication.class, args);
+	}
+}
